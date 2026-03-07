c2f6b01d4d18d7bc0d5d0dc1c3f499d534dd42d3
CYorkCS <calvinmyork@gmail.com>
2026-03-07 15:13:44 -0600

    Added KFold to get train_folds.csv

--- src/create_folds.py
+++ src/create_folds.py
@@ -1,2 +1,14 @@
-from sklearn.model_selection import StratifiedKFold
+import pandas as pd
+from sklearn.model_selection import KFold
 
+if __name__ == "__main__":
+    df = pd.read_csv("../input/train.csv")
+    df["kfold"] = -1
+    df.sample(frac=1).reset_index(drop=True)
+    
+    kf = KFold(n_splits=5)
+    
+    for fold, (trn_, val_) in enumerate(kf.split(X=df)):
+        df.loc[val_, 'kfold'] = fold
+    
+    df.to_csv("../input/train_folds.csv", index=False)
